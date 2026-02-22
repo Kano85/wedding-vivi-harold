b866e3148426523b85acdcbcf7042c4f57a9853a
Kano85 <afte_85@hotmail.com>
2026-02-22 16:18:00 +0100

    Update build version metadata

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2026-02-11T22:55:05.649Z
+// Build Time: 2026-02-22T15:14:52.983Z
 export const APP_VERSION = {
-  version: '1770850505649',
-  buildTime: '2026-02-11T22:55:05.649Z',
-  hash: 'f1vjbxyndrf'
+  version: '1771773292983',
+  buildTime: '2026-02-22T15:14:52.983Z',
+  hash: 'ycmefbbd06b'
 };
 
 export default APP_VERSION;
