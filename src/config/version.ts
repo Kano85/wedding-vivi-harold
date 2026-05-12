d824c4b96e20cbde5f5f14db514e3947ca01b691
Kano85 <afte_85@hotmail.com>
2026-05-12 23:14:43 +0200

    Update production build version

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2026-05-12T21:10:52.677Z
+// Build Time: 2026-05-12T21:14:18.291Z
 export const APP_VERSION = {
-  version: '1778620252677',
-  buildTime: '2026-05-12T21:10:52.677Z',
-  hash: 'hgjle6mxm4k'
+  version: '1778620458291',
+  buildTime: '2026-05-12T21:14:18.291Z',
+  hash: '03aabl4e78i'
 };
 
 export default APP_VERSION;
